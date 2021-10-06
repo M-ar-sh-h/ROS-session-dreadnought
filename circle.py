21fda42ecf3753ad4992a1aec26292c6493d2401
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-06 23:06:17 +0530

    turtle turtle go in a circle

---
+++ circle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def circle():
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+    rospy.init_node('turtle_turtle_go_in_a_circle', anonymous=False)
+    rate = rospy.Rate(1)
+    move = Twist()
+
+    while not rospy.is_shutdown():
+        move.linear.x = 5.0
+        move.angular.z = 2.0
+        pub.publish(move)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        circle()
+    except rospy.ROSInterruptException:
+        pass            
