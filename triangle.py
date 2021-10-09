6129bb108895c3d1c40042f025d4e718e7933ad2
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-09 22:47:16 +0530

    Turtle mad angle make a triangle

--- triangle.py
+++ triangle.py
@@ -3,15 +3,16 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
-PI = 3.1415926535897
+
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+rospy.init_node('turtle_rare_go_in_a_square', anonymous=False)
+rate = rospy.Rate(1)
+move = Twist()
+go_straight = 2*(math.pi)*90/360
+equilateral_angle = 2*(math.pi)*120/360
+
 
 def triangle():
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
-    rospy.init_node('turtle_rare_go_in_a_square', anonymous=False)
-    rate = rospy.Rate(1)
-    move = Twist()
-    go_straight = 2*(math.pi)*90/360
-    equilateral_angle = 2*(math.pi)*120/360
 
     while not rospy.is_shutdown():
         move.linear.x = 0.0
@@ -21,19 +22,11 @@ def triangle():
         
         for _ in range(4):
             move.linear.x = go_straight
-            move.linear.y = 0.0
-            move.linear.z = 0.0
-            move.angular.x = 0.0
-            move.angular.y = 0.0
             move.angular.z = 0.0
             pub.publish(move)
             rate.sleep()
 
             move.linear.x = 0.0
-            move.linear.y = 0.0
-            move.linear.z = 0.0
-            move.angular.x = 0.0
-            move.angular.y = 0.0
             move.angular.z = equilateral_angle
             pub.publish(move)
             rate.sleep()
