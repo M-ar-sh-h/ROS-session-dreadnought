50f9f51cfa9871b1d9ecaf5842e93432d26d12c0
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-09 22:45:51 +0530

    Turtle rare go in a square

--- square.py
+++ square.py
@@ -3,15 +3,15 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
-PI = 3.1415926535897
+
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+rospy.init_node('turtle_rare_go_in_a_square', anonymous=False)
+rate = rospy.Rate(1)
+move = Twist()
+go_straight = 2*(math.pi)*90/360
+right_angle = 2*(math.pi)*90/360
 
 def square():
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
-    rospy.init_node('turtle_rare_go_in_a_square', anonymous=False)
-    rate = rospy.Rate(1)
-    move = Twist()
-    go_straight = 2*(math.pi)*90/360
-    right_angle = 2*(math.pi)*90/360
 
     while not rospy.is_shutdown():
         move.linear.x = 0.0
@@ -21,19 +21,11 @@ def square():
         
         for _ in range(4):
             move.linear.x = go_straight
-            move.linear.y = 0.0
-            move.linear.z = 0.0
-            move.angular.x = 0.0
-            move.angular.y = 0.0
             move.angular.z = 0.0
             pub.publish(move)
             rate.sleep()
 
             move.linear.x = 0.0
-            move.linear.y = 0.0
-            move.linear.z = 0.0
-            move.angular.x = 0.0
-            move.angular.y = 0.0
             move.angular.z = right_angle
             pub.publish(move)
             rate.sleep()
