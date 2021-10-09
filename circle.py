2df301c28d4fa4546e7b3095873d2dc8ac914f03
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-09 22:48:17 +0530

    Turtle Turtle go in a circle

--- circle.py
+++ circle.py
@@ -3,11 +3,14 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+rospy.init_node('turtle_turtle_go_in_a_circle', anonymous=False)
+rate = rospy.Rate(1)
+move = Twist()
+
+
 def circle():
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
-    rospy.init_node('turtle_turtle_go_in_a_circle', anonymous=False)
-    rate = rospy.Rate(1)
-    move = Twist()
 
     while not rospy.is_shutdown():
         move.linear.x = 5.0
