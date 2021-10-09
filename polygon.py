e35e9899581a4678e9190e01adcafdb331738ac4
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-09 22:43:12 +0530

    Turtlesim polygon maker

---
+++ polygon.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import rospy
+import math
+import time
+from geometry_msgs.msg import Twist
+
+
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=1)
+rospy.init_node('turtle_mad_angle_make_a_triangle', anonymous=False)
+move = Twist()
+n = int(input("Number of sides: "))
+go_straight = 2*(math.pi)*90/360
+rotate_angle = 2*(math.pi)*(360/n)/360
+
+time.sleep(2)
+
+
+def straight():
+    move.linear.x = go_straight
+    move.angular.z = 0.0
+    pub.publish(move)
+    time.sleep(1)
+
+
+def rotate():
+    move.linear.x = 0.0
+    move.angular.z = rotate_angle
+    pub.publish(move)
+    time.sleep(1)
+
+
+def triangle():
+
+    move.linear.x = 0.0
+    move.angular.z = 0.0
+    pub.publish(move)
+    time.sleep(1)
+
+    while not rospy.is_shutdown():
+        for _ in range(n):
+            straight()
+            rotate()  
+        break    
+
+
+if __name__ == '__main__':
+    try:
+        triangle()
+    except rospy.ROSInterruptException:
+        pass
